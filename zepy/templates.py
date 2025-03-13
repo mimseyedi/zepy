d0070a031b007a4fa3250240ec7a61c3c6ba0f7c
Mohammad <mim.seyedi@gmail.com>
2025-03-13 18:02:04 +0330

    The "source" property name was changed to "template" and validation operations were implemented for the template.

--- zepy/templates.py
+++ zepy/templates.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 from string import Template
 
+from .errors import ProjectTemplateError
+
 
 class ProjectTemplate:
-    def __init__(self, source: Path, **tags) -> None:
-        self._source = source
+    def __init__(self, template: str, **tags) -> None:
+        self._template = self._get_template(template)
         self._tags = tags
 
     @property
-    def source(self) -> Path:
-        return self._source
+    def template(self) -> Path:
+        return self._template
 
     @property
     def tags(self) -> dict:
@@ -17,18 +19,18 @@ class ProjectTemplate:
 
     @property
     def name(self) -> str:
-        return self._source.name
+        return self._template.name
 
     def build(self, dest: Path) -> None:
         dest.mkdir(parents=True, exist_ok=True)
 
-        for item in self.source.rglob('*'):
+        for item in self.template.rglob('*'):
             if self._is_hidden_item(item):
                 continue
 
             relative = self.render(
                 item.relative_to(
-                    self.source
+                    self.template
                 ).__str__()
             )
             dest_item = Path(
@@ -54,6 +56,36 @@ class ProjectTemplate:
             template=string,
         ).safe_substitute(self.tags)
 
+    def _get_template(self, template: str) -> Path:
+        if (
+            t_path := Path(
+                Path(__file__).parent,
+                "templates",
+                template,
+            )
+        ).is_dir():
+            return self._template_validation(t_path)
+        else:
+            if (
+                t_path := Path(template)
+            ).is_dir():
+                return self._template_validation(t_path)
+            raise ProjectTemplateError(
+                "Error: No template was found in this path."
+            )
+
+    @staticmethod
+    def _template_validation(template_path: Path) -> Path:
+        if not Path(
+            template_path,
+            "settings.yaml",
+        ).is_file():
+            raise ProjectTemplateError(
+                "Error: Template is corrupted.\n"
+                "\t- The 'settings.yaml' file was not found."
+            )
+        return template_path
+
     @staticmethod
     def _is_hidden_item(item: Path) -> bool:
         if item.name.startswith('.'):
@@ -66,9 +98,9 @@ class ProjectTemplate:
     def __repr__(self) -> str:
         return (
             f"ProjectTemplate("
-            f"{self.source},"
+            f"{self.template},"
             f"{self.tags})"
         )
 
     def __str__(self) -> str:
-        return f"ProjectTemplate({self.source})"
+        return f"ProjectTemplate({self.template})"
