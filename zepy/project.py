7e31d6957d8080118d5c53ade35d3d2b2865f36c
Mohammad <mim.seyedi@gmail.com>
2025-03-13 13:47:37 +0330

    The signature and prototype of the "Project" class were implemented.

--- zepy/project.py
+++ zepy/project.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import (
     Any,
     Dict,
+    Optional,
 )
 from pathlib import Path
 
@@ -25,4 +26,30 @@ class ProjectSettings:
         ...
 
     def __str__(self) -> str:
+        ...
+
+
+class Project:
+    def __init__(self, settings: ProjectSettings) -> None:
+        self._settings = settings
+
+    @property
+    def settings(self) -> ProjectSettings:
+        return self._settings
+
+    @classmethod
+    def new(
+        cls,
+        root: Path,
+        template: Path,
+        *,
+        description: Optional[str] = None,
+        python_version: Optional[str] = None,
+        license_: Optional[str] = None,
+        user: Optional[str] = None,
+    ) -> Project:
+        ...
+
+    @classmethod
+    def open(cls, root: Path) -> Project:
         ...
